a95877adeda87989f759a201d9a593777b1982eb
tksgtkm <h9cd342fd7jgx36@gmail.com>
2022-04-16 10:00:42 +0900

    gitignore second commit

---
+++ web_server/business_card/components/bizcard/bizcard.js
@@ -0,0 +1,18 @@
+import Template from './template.js';
+
+class BizCard extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = Template.render({
+      first_name: "Takumi",
+      last_name: 'Takasugi',
+      title: 'Student of all Sciences',
+      phone: '080-3834-1377',
+      email: 'hhhhoooo@gmail.com',
+      website: 'www.imgod.com'
+    });
+  }
+}
+
+if (!customElements.get('biz-card')) {
+  customElements.define('biz-card', BizCard);
+}
